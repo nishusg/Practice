90b1bb55912d29ef2e7c9e0046404297dcbebdcf
Nishant <nishantsg218@gmail.com>
2022-04-07 11:06:38 +0530

    update button

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 let a = 0;
 let b = null;
-let i = 0;
 let start = document.getElementById("start");
 let stop = document.getElementById("stop");
 let restart = document.getElementById("restart");
 start.addEventListener("click", () => {
-  i = i + 1;
-  if (i == 1) {
-    b = setInterval(() => {
-      a++;
-      document.getElementById("display").innerHTML = a;
-    }, 1000);
-  }
+  start.disabled = true;
+  b = setInterval(() => {
+    a++;
+    document.getElementById("display").innerHTML = a;
+  }, 1000);
 });
 
 stop.addEventListener("click", () => {
-  i = 0;
+  start.disabled = false;
   clearInterval(b);
   a = document.getElementById("display").innerText;
 });
 
 restart.addEventListener("click", () => {
-  i = 0;
+  start.disabled = false;
   a = 0;
   clearInterval(b);
   document.getElementById("display").innerHTML = a;
